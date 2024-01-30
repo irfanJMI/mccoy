53dda2cc3af569ba1196f90a40a4c57801bf5554
irfanJMI <irfanali.k@hotmail.co.uk>
2024-01-30 18:51:56 +0530

    craeted components

---
+++ org/src/left-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'org-left-panel',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './left-panel.component.html',
+  styleUrl: './left-panel.component.css',
+})
+export class LeftPanelComponent {}
---
+++ org/src/mail-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'org-mail-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './mail-page.component.html',
+  styleUrl: './mail-page.component.css',
+})
+export class MailPageComponent {}
